52b62d394f9b19fa34974f2748ace87a7fa69f72
Aniket Patel <aniketupatel1@gmail.com>
2019-02-11 16:44:06 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -1,9 +1,10 @@
 var index = require("./index.js")
+
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
-}var index = require("./index.js")
+}
 
 if (index.name === "Susan") {
     console.log("The name is correct")
